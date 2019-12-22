116e80dbbb9649ba0cbf91c1b0d1ad13e6730585
AnastasiiaTyryshkina <tyrnasmail@gmail.com>
2019-12-22 15:31:06 +0300

    Updates

---
+++ precache-manifest.7b47166cfc9b13b4f15ca5a8deb7b620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5e73c755e4a3d1efc70c670df123da",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "22868099d76759499990",
+    "url": "/testReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb00de41366120efa94f",
+    "url": "/testReact/static/js/2.78313752.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testReact/static/js/2.78313752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22868099d76759499990",
+    "url": "/testReact/static/js/main.23bcda09.chunk.js"
+  },
+  {
+    "revision": "baa78607cbac39f6844f",
+    "url": "/testReact/static/js/runtime-main.ddae3f0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
